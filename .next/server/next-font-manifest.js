4af7aea194961385a3a7eb1d15ad92f21395a874
Danyal Imran <dan.imran97@gmail.com>
2024-03-12 15:32:45 -0400

    set up project foundations;

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/danimran/Desktop/desktop items/Fire Projects Only/hydrohomie/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+qMshvVrz+i9ChrDw1e1Iv7LJgdvJIg7gTE2fBFbtak=\"\n}"
